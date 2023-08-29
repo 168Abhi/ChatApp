c03d751a4fa871f42ef8778a3ada375e2e3d7be8
168Abhi <abhishukla168@gmail.com>
2023-08-29 21:30:52 +0530

    hii

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">TalkIt</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
